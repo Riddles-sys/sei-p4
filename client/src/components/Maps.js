c932db171cc0c595fc997a897530c6e9f9bf2d4d
Riddles <ridwan.arshad@hotmail.com>
2022-09-20 16:22:47 +0100

    styling

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -4,7 +4,7 @@ const Maps = () => {
  
   return (
     <div className='maps-page container '>
-      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <div className="container-map mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
         <h1 className="title text-3xl text-gray-700 font-bold mb-5">
         Welcome to Maps</h1>              
       </div>
