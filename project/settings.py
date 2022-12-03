b45b7ffe89d3687d3dc0dc479a50d9caf1a7aaa8
Riddles <ridwan.arshad@hotmail.com>
2022-12-03 21:04:03 +0000

    changed allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ["*"]
 
 
 
@@ -156,7 +156,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'client', "build", "static"),
 )
 
-ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = ['.vercel.app']
 
 django_on_heroku.settings(locals()) # put this last
 
